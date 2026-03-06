a6bb36e7165def10e2123c0c90ae5dc05c57a545
bush <jjjbushjjj@gmail.com>
2026-03-07 00:13:30 +0300

    add markdown plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -34,7 +34,7 @@ vim.pack.add({
 { src = "https://github.com/nvim-mini/mini.statusline" },
 { src = "https://github.com/kdheepak/lazygit.nvim" },
 { src = "https://github.com/stevearc/oil.nvim.git" },
-
+{ src = "https://github.com/MeanderingProgrammer/render-markdown.nvim" },
 })
 
 vim.diagnostic.config ({
